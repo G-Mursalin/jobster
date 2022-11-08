71837022447850bd7fce6d4d875d3f47a010aac1
Golam Mursalin <golammursalin309@gmail.com>
2022-11-08 11:38:51 +0600

    landing page added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+// React
+import React from "react";
+// React Router
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// Pages
+import Landing from "./pages/Landing";
+import Dashboard from "./pages/Dashboard";
+import Register from "./pages/Register";
+import Error from "./pages/Error";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Dashboard />}></Route>
+          <Route path="landing" element={<Landing />}></Route>
+          <Route path="register" element={<Register />}></Route>
+          <Route path="*" element={<Error />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </React.Fragment>
   );
-}
+};
 
 export default App;
