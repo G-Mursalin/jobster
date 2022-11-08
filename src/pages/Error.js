03af822a00af714c5e9797c83f31e42142cf5f80
Golam Mursalin <golammursalin309@gmail.com>
2022-11-08 11:50:07 +0600

    react router and error page added

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,7 +1,22 @@
+// React
 import React from "react";
+// React Router
+import { Link } from "react-router-dom";
+// Images
+import img from "./../assets/images/not-found.svg";
+import Wrapper from "./../assets/wrappers/ErrorPage";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <Wrapper>
+      <div>
+        <img src={img} alt="not found" />
+        <h3>Ohh! Page Not Found</h3>
+        <p>We can't seem to find the page you're looking for</p>
+        <Link to="/">Back to home</Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Error;
