f9e417bd7d89caf26bf28d6d1e7110f7584c4f4b
Golam Mursalin <golammursalin309@gmail.com>
2022-11-19 22:54:49 +0600

    big side bar added

--- src/components/BigSideBar.js
+++ src/components/BigSideBar.js
@@ -2,11 +2,30 @@
 import React from "react";
 // CSS
 import Wrapper from "./../assets/wrappers/BigSidebar";
+// React Redux
+import { useSelector } from "react-redux";
+//components
+import NavLinks from "./NavLinks";
+import Logo from "../components/Logo";
 
 const BigSideBar = () => {
+  const { isSidebarOpen } = useSelector((store) => store.user);
   return (
     <Wrapper>
-      <h2>BigSideBar</h2>
+      <div
+        className={
+          isSidebarOpen
+            ? "sidebar-container "
+            : "sidebar-container show-sidebar"
+        }
+      >
+        <div className="content">
+          <header>
+            <Logo />
+          </header>
+          <NavLinks />
+        </div>
+      </div>
     </Wrapper>
   );
 };
